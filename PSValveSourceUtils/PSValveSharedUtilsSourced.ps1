d35664720f8a38dc69828b1ab7b422a614e2a588
El_Daro <el_daro@outlook.com>
2025-04-18 13:35:06 +0300

    Refactored config functions into their own module: PSResourceParser

---
+++ PSValveSourceUtils/PSValveSharedUtilsSourced.ps1
@@ -0,0 +1,10 @@
+$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -Recurse -ErrorAction SilentlyContinue )
+
+foreach ($import in @($Public + $Private)) {
+	try {
+		. $import.FullName
+	} catch {
+		Write-Error -Message "Failed to import function $($import.FullName): $_"
+	}
+}
